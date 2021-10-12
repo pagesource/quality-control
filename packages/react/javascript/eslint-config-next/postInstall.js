06e92e5df0fd4e53e089628e4046babc60cc2ed2
ayapaul <ayanava.paul@publicissapient.com>
2021-10-12 11:46:11 +0530

    fix(eslint-config-next): added feature to auto create file .eslintrc.js
    Added feature to auto create file .eslintrc.js
    Added documentation for the package.

--- packages/react/javascript/eslint-config-next/postInstall.js
+++ packages/react/javascript/eslint-config-next/postInstall.js
@@ -4,8 +4,11 @@ const fs = require('fs');
 const path = require('path');
 const npm = require('npm');
 const dirPath = path.resolve(__dirname).split('/node_modules')[0];
+const eslintfile = `${dirPath}/.eslintrc.js`
 const targetFileName = `${dirPath}/package.json`;
 const sourcePackage = fs.readFileSync('package.json');
+
+const content = fs.readFileSync('index.js', {encoding:'utf8', flag:'r'})
 
 /* Installing peer dependencies packages  here */
 
@@ -19,6 +22,15 @@ const installPackages = (path, depArr) => {
     }
   });
 };
+
+const cretaeLintFile = () => {
+  try {
+    fs.writeFileSync(`${dirPath}/.eslintrc.js`, content)
+    console.log('file written successfully')
+  } catch (err) {
+    console.error(err)
+  }
+}
 
 /* creating peer dependencies array from peer dependencies object */
 
@@ -38,6 +50,7 @@ const getDepArray = (peerDep) => {
 const installPeerDependencies = (peerDep) => {
   console.info(`installing ${Object.keys(peerDep).length || 0} packages`);
   const depArr = getDepArray(peerDep);
+  console.log('dependency arr', depArr)
   installPackages(dirPath, depArr);
 };
 
@@ -49,7 +62,7 @@ const updateDependencies = () => {
     targetJson = targetPackage && JSON.parse(targetPackage);
 
   const peerDeppendency = sourceJson.peerDependencies;
-
+  console.log('peer dependency', peerDeppendency)
   if (sourceJson && targetJson) {
     if (targetJson.hasOwnProperty('devDependencies')) {
       targetJson.devDependencies = Object.assign(
@@ -78,3 +91,4 @@ const updateDependencies = () => {
 };
 
 updateDependencies();
+cretaeLintFile();
